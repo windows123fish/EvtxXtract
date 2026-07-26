076f369fee5ddc243d75fcf57fb6b48fdb923b41
windows123fish <xhoddh@outlook.com>
2026-07-26 17:13:44 +0800

    dev: update 周日 2-26-07_17:13:43

--- src/gui_window.cpp
+++ src/gui_window.cpp
@@ -1,4 +1,13 @@
 #include "gui_window.h"
 #include "evtx_parser.h"
 #include <imgui.h>
-#include <imgui
+#include <imgui_impl_win32.h>
+#include <imgui_impl_dx11.h>
+#include <d3d11.h>
+#include <windows.h>
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <algorithm>
+
+#pragma comment(lib
