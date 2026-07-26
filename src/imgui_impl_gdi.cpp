b21b7c7cb3ac44fad74b6aa0e0a1c20f9aaba226
windows123fish <xhoddh@outlook.com>
2026-07-26 16:35:33 +0800

    dev: update 周日 2-26-07_16:35:32

--- src/imgui_impl_gdi.cpp
+++ src/imgui_impl_gdi.cpp
@@ -165,4 +165,5 @@ IMGUI_IMPL_API void ImGui_ImplGDI_RenderDrawData(ImDrawData* draw_data) {
                         // Barycentric coordinates
                         float w0 = ((v1.y - v2.y) * (x - v2.x) + (v2.x - v1.x) * (y - v2.y)) /
                                    ((v1.y - v2.y) * (v0.x - v2.x) + (v2.x - v1.x) * (v0.y - v2.y));
-                        float w1 = ((v2.y - v0.y) * (x - v2.x) + (v0.x - v
+                        float w1 = ((v2.y - v0.y) * (x - v2.x) + (v0.x - v2.x) * (y - v2.y)) /
+                                   ((v1.y - v
