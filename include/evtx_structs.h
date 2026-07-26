b805048359427605fb9c44a51d1fd7da29255080
windows123fish <xhoddh@outlook.com>
2026-07-26 15:10:59 +0800

    dev: update 周日 2-26-07_15:10:58

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -117,4 +117,7 @@ struct EVT_FILE_HEADER {
    * 
    * @return Major version
    */
-  uint16_t get_m
+  uint16_t get_major_version() const noexcept;
+
+  /**
+   * @brief Get the minor version number
