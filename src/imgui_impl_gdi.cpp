75d8d64820752eb335b283227ba3ed2131e77a1b
windows123fish <xhoddh@outlook.com>
2026-07-26 16:35:36 +0800

    dev: update 周日 2-26-07_16:35:35

--- src/imgui_impl_gdi.cpp
+++ src/imgui_impl_gdi.cpp
@@ -173,4 +173,5 @@ IMGUI_IMPL_API void ImGui_ImplGDI_RenderDrawData(ImDrawData* draw_data) {
                             float r = w0 * c0.x + w1 * c1.x + w2 * c2.x;
                             float g = w0 * c0.y + w1 * c1.y + w2 * c2.y;
                             float b = w0 * c0.z + w1 * c1.z + w2 * c2.z;
-                            float a = w0 *
+                            float a = w0 * c0.w + w1 * c1.w + w2 * c2.w;
+
