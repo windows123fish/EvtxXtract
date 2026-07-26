fa20cd9c5c877654fcd1ce9c15cbe612005c04e1
windows123fish <xhoddh@outlook.com>
2026-07-26 16:38:29 +0800

    dev: update 周日 2-26-07_16:38:28

--- src/gui_window.cpp
+++ src/gui_window.cpp
@@ -238,4 +238,6 @@ void GuiWindow::renderFileInfo() {
                 ImGui::TableSetColumnIndex(1);
                 ImGui::Text("%u", info.event_count);
                 ImGui::TableSetColumnIndex(2);
-                ImGui::Text("0x%
+                ImGui::Text("0x%08X", info.checksum);
+            }
+            ImGui::EndTable();
