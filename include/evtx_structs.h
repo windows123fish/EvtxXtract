1713bad4e11f2d0975378b150e5d6c1ba0300b0b
windows123fish <xhoddh@outlook.com>
2026-07-26 15:29:41 +0800

    dev: update 周日 2-26-07_15:29:40

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -158,4 +158,5 @@ struct EVT_FILE_HEADER {
  * 0x003C | 4    | flags
  * 0x0040 | 4    | chunk_checksum
  * 0x0044 | 4    | string_offset_array_offset
- *
+ * 0x0048 | 440  | reserved
+ * -------|------
