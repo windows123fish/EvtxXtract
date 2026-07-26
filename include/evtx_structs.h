03116d11c27bef86539d29e92c613a576a48111e
windows123fish <xhoddh@outlook.com>
2026-07-26 15:11:15 +0800

    dev: update 周日 2-26-07_15:11:14

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -185,4 +185,8 @@ struct EVT_CHUNK_HEADER {
   /**
    * @brief Last event record number in this chunk (8 bytes, little-endian)
    */
-  uint64_t last_event
+  uint64_t last_event_record_number;
+
+  /**
+   * @brief First event record identifier in this chunk (8 bytes, little-endian)
+   */
