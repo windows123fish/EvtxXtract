0af91e814b6922bd70cd290f26265fee40ec3fb8
windows123fish <xhoddh@outlook.com>
2026-07-26 15:26:06 +0800

    dev: update 周日 2-26-07_15:26:06

--- src/debug_header.cpp
+++ src/debug_header.cpp
@@ -92,3 +92,5 @@ int main(int argc, char* argv[]) {
   uint32_t checksum = *reinterpret_cast<uint32_t*>(&buffer[0x28]);
   std::cout << "[0x0028-0x002B] Checksum: 0x" << std::hex << checksum << "\n";
   
+  return 0;
+}
