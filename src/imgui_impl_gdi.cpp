a332a3e59566201b8524838c4f5e076cc83a80a4
windows123fish <xhoddh@outlook.com>
2026-07-26 16:34:59 +0800

    dev: update 周日 2-26-07_16:34:58

--- src/imgui_impl_gdi.cpp
+++ src/imgui_impl_gdi.cpp
@@ -5,4 +5,5 @@ static HDC              g_hDC = NULL;
 static HBITMAP          g_hBitmap = NULL;
 static HBITMAP          g_hBitmapOld = NULL;
 static int              g_Width = 0;
-static
+static int              g_Height = 0;
+static unsigned char*   g_Buffer = NULL
