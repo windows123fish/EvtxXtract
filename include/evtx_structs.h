c9c2cf4f2346dee27dbf5630cdd3311422e85be2
windows123fish <xhoddh@outlook.com>
2026-07-26 15:29:37 +0800

    dev: update 周日 2-26-07_15:29:37

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -149,4 +149,5 @@ struct EVT_FILE_HEADER {
  * 0x0008 | 8    | first_event_record_number
  * 0x0010 | 8    | last_event_record_number
  * 0x0018 | 8    | first_event_record_id
- * 0x00
+ * 0x0020 | 8    | last_event_record_id
+ * 0x0028
