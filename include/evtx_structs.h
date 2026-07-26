302fd767919962cdb22a9810e5218b86c5e55a8f
windows123fish <xhoddh@outlook.com>
2026-07-26 15:23:52 +0800

    dev: update 周日 2-26-07_15:23:52

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -152,4 +152,6 @@ struct EVT_FILE_HEADER {
  * 0x0020 | 8    | last_event_record_id
  * 0x0028 | 4    | header_size (always 128 = 0x80)
  * 0x002C | 4    | last_event_offset
- * 0x0030 | 4    | free_space
+ * 0x0030 | 4    | free_space_offset
+ * 0x0034 | 4    | events_checksum
+ *
