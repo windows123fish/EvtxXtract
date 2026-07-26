da3d28613fb8cdd4a8cd9ccf9a830ef68d9f7005
windows123fish <xhoddh@outlook.com>
2026-07-26 16:34:06 +0800

    dev: update 周日 2-26-07_16:34:05

--- src/imgui_impl_win32.cpp
+++ src/imgui_impl_win32.cpp
@@ -2,4 +2,8 @@
 #include <windows.h>
 
 // Forward declarations
-extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+static HWND                 g_hWnd = NULL;
+static HDC                  g_hDC = NULL;
+static ImGuiMouseCursor     g_LastMouseCursor =
