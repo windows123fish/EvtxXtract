9e2b8482315bfa8d5d045aa9cc4af3737b4aff76
windows123fish <xhoddh@outlook.com>
2026-07-26 15:10:52 +0800

    dev: update 周日 2-26-07_15:10:51

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -75,4 +75,7 @@ struct EVT_FILE_HEADER {
   /**
    * @brief File size in bytes (8 bytes, little-endian)
    */
-  uint64_t
+  uint64_t file_size;
+
+  /**
+   * @brief Offset of the oldest chunk in the file (
