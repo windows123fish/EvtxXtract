ac179ec653dcab5be505a4ce4f5c57b17e5d68ed
windows123fish <xhoddh@outlook.com>
2026-07-26 16:37:12 +0800

    dev: update 周日 2-26-07_16:37:09

--- src/gui_main.cpp
+++ src/gui_main.cpp
@@ -6,3 +6,7 @@ GuiWindow* g_pWindow = nullptr;
 
 // WinMain entry point
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    // Create window
+    g_pWindow = new GuiWindow();
+    
+    if (!g_pWindow
