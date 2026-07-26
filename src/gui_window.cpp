fdbc0abc39ead18f7ec38d21a57d3a5a0bfb3bcc
windows123fish <xhoddh@outlook.com>
2026-07-26 16:35:48 +0800

    dev: update 周日 2-26-07_16:35:48

--- src/gui_window.cpp
+++ src/gui_window.cpp
@@ -11,4 +11,5 @@ extern IMGUI_IMPL_API bool ImGui_ImplWin32_Init(HWND hWnd);
 extern IMGUI_IMPL_API void ImGui_ImplWin32_Shutdown();
 extern IMGUI_IMPL_API void ImGui_ImplWin32_NewFrame();
 extern IMGUI_IMPL_API bool ImGui_ImplGDI_Init(HDC hDC);
-extern IMGUI_IMPL_API void ImGui_ImplGDI_Sh
+extern IMGUI_IMPL_API void ImGui_ImplGDI_Shutdown();
+extern IMGUI_IMPL_API void ImGui_ImplGDI_RenderDrawData(
