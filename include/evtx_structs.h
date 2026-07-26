8cde6ab0bf688dcc11853bd2b0b708eb7d4bbf6e
windows123fish <xhoddh@outlook.com>
2026-07-26 15:34:05 +0800

    dev: update 周日 2-26-07_15:34:04

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -56,3 +56,4 @@ constexpr std::array<uint8_t, 8> EVTX_FILE_MAGIC = {
     'E', 'l', 'f', 'F', 'i', 'l', 'e', 0x00
 };
 constexpr std::array<uint8_t, 8> EVTX_CHUNK_MAGIC = {
+    'E', 'l', 'f', 'C', 'h', 'n', 'k
