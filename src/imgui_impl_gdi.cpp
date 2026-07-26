189d1878e88b82ce411b616e83a5caac4a9c6891
windows123fish <xhoddh@outlook.com>
2026-07-26 16:34:59 +0800

    dev: update 周日 2-26-07_16:34:59

--- src/imgui_impl_gdi.cpp
+++ src/imgui_impl_gdi.cpp
@@ -6,4 +6,7 @@ static HBITMAP          g_hBitmap = NULL;
 static HBITMAP          g_hBitmapOld = NULL;
 static int              g_Width = 0;
 static int              g_Height = 0;
-static unsigned char*   g_Buffer = NULL
+static unsigned char*   g_Buffer = NULL;
+
+// Initialize GDI rendering
+IMGUI_IMPL_API bool ImGui_ImplGDI_Init
