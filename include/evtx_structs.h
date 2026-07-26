432440ae3ea3f9344db9a9a3dbbebbfc5d744257
windows123fish <xhoddh@outlook.com>
2026-07-26 15:34:57 +0800

    dev: update 周日 2-26-07_15:34:57

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -24,4 +24,7 @@ namespace Evtx {
  * 0x0018 | 8    | oldest_chunk_offset
  * 0x0020 | 8    | newest_chunk_offset
  * 0x0028 | 4    | checksum
- * 0x002C | 4052 |
+ * 0x002C | 4052 | reserved
+ */
+struct EVT_FILE_HEADER {
+  std::array<uint8_t,
