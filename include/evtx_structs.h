d114999c28c75326e462c6bd73ad2e90f379532b
windows123fish <xhoddh@outlook.com>
2026-07-26 15:31:24 +0800

    dev: update 周日 2-26-07_15:31:23

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -87,3 +87,6 @@ struct EVT_FILE_HEADER {
   bool validate_magic() const noexcept;
   uint16_t get_major_version() const noexcept;
   uint16_t get_minor_version() const noexcept;
+  bool is_dirty() const noexcept;
+  std::string to_string() const;
+};
