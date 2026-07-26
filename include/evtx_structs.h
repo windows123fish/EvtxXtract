5de7e088162bc8f007579bde126d68f31cbe1f6f
windows123fish <xhoddh@outlook.com>
2026-07-26 15:24:01 +0800

    dev: update 周日 2-26-07_15:24:01

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -198,4 +198,6 @@ struct EVT_CHUNK_HEADER {
   uint32_t header_size;
 
   /**
-   * @brief Offset of the last event record
+   * @brief Offset of the last event record in this chunk (4 bytes, little-endian)
+   */
+  uint32_t
