862cb8bfaac75b5a9c88ab974d9e33c198cec4bf
windows123fish <xhoddh@outlook.com>
2026-07-26 15:31:33 +0800

    dev: update 周日 2-26-07_15:31:32

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -128,4 +128,6 @@ struct EVT_CHUNK_HEADER {
   uint32_t events_checksum;
   uint32_t unknown1;
   uint32_t flags;
-  uint32_t
+  uint32_t chunk_checksum;
+  uint32_t string_offset_array_offset;
+  std::array
