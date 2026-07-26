d6506313d19c9e5991cb01025f3c95367c4ea187
windows123fish <xhoddh@outlook.com>
2026-07-26 15:26:01 +0800

    dev: update 周日 2-26-07_15:26:00

--- src/debug_header.cpp
+++ src/debug_header.cpp
@@ -82,4 +82,4 @@ int main(int argc, char* argv[]) {
   
   // Oldest Chunk Offset (0x0018, 8 bytes)
   uint64_t oldest_offset = *reinterpret_cast<uint64_t*>(&buffer[0x18]);
-  std
+  std::cout << "[0x0018
