8a0d198008e5f804ff5461deb52d39a0e8004a64
windows123fish <xhoddh@outlook.com>
2026-07-26 16:35:54 +0800

    dev: update 周日 2-26-07_16:35:54

--- src/gui_window.cpp
+++ src/gui_window.cpp
@@ -48,4 +48,6 @@ bool GuiWindow::init(HINSTANCE hInstance, const std::string& title, int width, i
     }
 
     RECT rect = {0, 0, width, height};
-    AdjustWindowRectEx(&rect, WS_OVERLAPP
+    AdjustWindowRectEx(&rect, WS_OVERLAPPEDWINDOW, FALSE, 0);
+    
+    m_hWnd = CreateWindowExA(
