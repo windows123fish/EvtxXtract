2cad442335b16f618e1d305918e116c43837406a
windows123fish <xhoddh@outlook.com>
2026-07-26 15:31:35 +0800

    dev: update 周日 2-26-07_15:31:34

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -141,4 +141,6 @@ struct EVT_CHUNK_HEADER {
 // Constants
 constexpr size_t EVTX_FILE_HEADER_SIZE = 4096;
 constexpr size_t EVTX_CHUNK_SIZE = 65536;
-constexpr
+constexpr size_t EVTX_CHUNK_HEADER_SIZE = 512;
+
+// Expected magic values
