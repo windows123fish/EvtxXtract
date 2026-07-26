0b4df43be8ae31ed098a30ffd222866e19995147
windows123fish <xhoddh@outlook.com>
2026-07-26 16:35:24 +0800

    dev: update 周日 2-26-07_16:35:23

--- src/imgui_impl_gdi.cpp
+++ src/imgui_impl_gdi.cpp
@@ -138,3 +138,6 @@ IMGUI_IMPL_API void ImGui_ImplGDI_RenderDrawData(ImDrawData* draw_data) {
             clip_rect.right = (LONG)pcmd->ClipRect.z;
             clip_rect.bottom = (LONG)pcmd->ClipRect.w;
             IntersectClipRect(g_hDC, clip_rect.left, clip_rect.top, clip_rect.right, clip_rect.bottom);
+
+            // Draw indexed triangles
+            for (int i = 0; i < pcmd
