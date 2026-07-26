6968518ed2a1717df417a92b7846f08a8503efbe
windows123fish <xhoddh@outlook.com>
2026-07-26 15:29:34 +0800

    dev: update 周日 2-26-07_15:29:33

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -137,4 +137,5 @@ struct EVT_FILE_HEADER {
 /**
  * @brief EVT_CHUNK_HEADER structure
  * 
- * Represents the 512
+ * Represents the 512-byte header at the beginning of each 64KB chunk.
+ * The magic number "
