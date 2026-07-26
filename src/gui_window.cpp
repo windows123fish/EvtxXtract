571e16b4488d61b5111651737f6a4140f0c607ae
windows123fish <xhoddh@outlook.com>
2026-07-26 16:49:54 +0800

    dev: update 周日 2-26-07_16:49:53

--- src/gui_window.cpp
+++ src/gui_window.cpp
@@ -228,4 +228,5 @@ void GuiWindow::renderDrawData(ImDrawData* draw_data) {
                         float w0 = ((v1.y - v2.y) * (x - v2.x) + (v2.x - v1.x) * (y - v2.y)) / 
                                    ((v1.y - v2.y) * (v0.x - v2.x) + (v2.x - v1.x) * (v0.y - v2.y));
                         float w1 = ((v2.y - v0.y) * (x - v2.x) + (v0.x - v2.x) * (y - v2.y)) / 
-                                   ((v1.y - v2.y) * (v0.x - v2.x) + (v2.x - v1
+                                   ((v1.y - v2.y) * (v0.x - v2.x) + (v2.x - v1.x) * (v0.y - v2.y));
+                        float w2 = 1
