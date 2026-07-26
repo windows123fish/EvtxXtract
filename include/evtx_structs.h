84508d251a6a3bca1f4c44047aa9e6c525fbe508
windows123fish <xhoddh@outlook.com>
2026-07-26 15:24:13 +0800

    dev: update 周日 2-26-07_15:24:12

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -271,4 +271,5 @@ constexpr std::array<uint8_t, 8> EVTX_CHUNK_MAGIC = {
     'E', 'l', 'f', 'C', 'h', 'n', 'k', 0x00
 };
 
-// Compile-time
+// Compile-time size validation
+static_assert(sizeof(EVT_FILE_HEADER) == EVTX_FILE_HEADER_SIZE,
