d9a77e31f3e733f92b9738c50f23868a98a066e6
windows123fish <xhoddh@outlook.com>
2026-07-26 16:35:10 +0800

    dev: update 周日 2-26-07_16:35:10

--- src/imgui_impl_gdi.cpp
+++ src/imgui_impl_gdi.cpp
@@ -79,4 +79,6 @@ static bool ImGui_ImplGDI_CreateBuffer(int width, int height) {
 }
 
 // Render ImGui draw data to GDI
-IMGUI_IMPL_API void ImGui_ImplGDI_RenderDrawData(ImDrawData*
+IMGUI_IMPL_API void ImGui_ImplGDI_RenderDrawData(ImDrawData* draw_data) {
+    if (!g_hDC || !draw_data)
+        return;
