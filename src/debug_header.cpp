024d3308dd9817806964eaa335bcf17d7608e017
windows123fish <xhoddh@outlook.com>
2026-07-26 15:25:36 +0800

    dev: update 周日 2-26-07_15:25:35

--- src/debug_header.cpp
+++ src/debug_header.cpp
@@ -5,4 +5,6 @@
 
 void print_hex_dump(const uint8_t* data, size_t size) {
   for (size_t i = 0; i < size; i += 16) {
-    std::cout << std::hex << std::setw(8) << std::setfill('0')
+    std::cout << std::hex << std::setw(8) << std::setfill('0') << i << "  ";
+    
+    // Print
