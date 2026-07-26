09f23872e05db416397c11ada1c7ad35df332324
windows123fish <xhoddh@outlook.com>
2026-07-26 15:23:47 +0800

    dev: update 周日 2-26-07_15:23:47

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -128,4 +128,7 @@ struct EVT_FILE_HEADER {
 
   /**
    * @brief Get a human-readable description of the header
-   *
+   * 
+   * @return String containing header information
+   */
+  std::string to_string()
