6487082156fc3d202afc9a69b374291ba9e1f06f
windows123fish <xhoddh@outlook.com>
2026-07-26 16:37:45 +0800

    dev: update 周日 2-26-07_16:37:44

--- src/gui_window.cpp
+++ src/gui_window.cpp
@@ -10,4 +10,5 @@
 extern IMGUI_IMPL_API bool ImGui_ImplWin32_Init(HWND hWnd);
 extern IMGUI_IMPL_API void ImGui_ImplWin32_Shutdown();
 extern IMGUI_IMPL_API void ImGui_ImplWin32_NewFrame();
-extern IMGUI_IMPL_API bool ImGui_ImplGDI_Init(HDC
+extern IMGUI_IMPL_API bool ImGui_ImplGDI_Init(HDC hDC);
+extern IMGUI_IMPL_API void ImGui_ImplGDI_Shutdown();
