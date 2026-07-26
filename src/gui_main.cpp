f4477be594b9795918bee770ec89e8b0245241a6
windows123fish <xhoddh@outlook.com>
2026-07-26 16:37:14 +0800

    dev: update 周日 2-26-07_16:37:13

--- src/gui_main.cpp
+++ src/gui_main.cpp
@@ -28,3 +28,6 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
         case DLL_PROCESS_ATTACH:
         case DLL_THREAD_ATTACH:
         case DLL_THREAD_DETACH:
+        case DLL_PROCESS_DETACH:
+            break;
+    }
