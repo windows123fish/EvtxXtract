3115c7362fdde57540844510dabeb9c2b0aff827
windows123fish <xhoddh@outlook.com>
2026-07-26 16:37:09 +0800

    dev: update 周日 2-26-07_16:37:09

--- src/gui_main.cpp
+++ src/gui_main.cpp
@@ -1,1 +1,8 @@
-#include <windows.h
+#include <windows.h>
+#include "gui_window.h"
+
+// Global window instance
+GuiWindow* g_pWindow = nullptr;
+
+// WinMain entry point
+int WINAPI WinMain(HINSTANCE hInstance,
