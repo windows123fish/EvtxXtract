670d0bbb624ed2f3ab230cb89d4332db17b214c2
windows123fish <xhoddh@outlook.com>
2026-07-26 15:29:54 +0800

    dev: update 周日 2-26-07_15:29:53

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -226,3 +226,8 @@ struct EVT_CHUNK_HEADER {
    * @brief CRC32 checksum of the chunk header (4 bytes, little-endian)
    * 
    * Computed over bytes 0x0008 through 0x01FF of the header
+   */
+  uint32_t chunk_checksum;
+
+  /**
+   * @brief
