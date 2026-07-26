624879c6f8e13fb8ebace2e608dc06182ccf78ea
windows123fish <xhoddh@outlook.com>
2026-07-26 16:40:36 +0800

    dev: update 周日 2-26-07_16:40:35

---
+++ build/CMakeFiles/EvtxXtract.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for EvtxXtract.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/debug_header.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/debug_header.dir/src/debug_header.cpp.obj"
+  "CMakeFiles/debug_header.dir/src/debug_header.cpp.obj.d"
+  "debug_header.exe"
+  "debug_header.exe.manifest"
+  "debug_header.pdb"
+  "libdebug_header.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/debug_header.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
