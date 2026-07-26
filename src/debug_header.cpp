02a432a2f407e942263a86dc886720481601e4de
windows123fish <xhoddh@outlook.com>
2026-07-26 15:26:04 +0800

    dev: update 周日 2-26-07_15:26:03

--- src/debug_header.cpp
+++ src/debug_header.cpp
@@ -86,4 +86,6 @@ int main(int argc, char* argv[]) {
   
   // Newest Chunk Offset (0x0020, 8 bytes)
   uint64_t newest_offset = *reinterpret_cast<uint64_t*>(&buffer[0x20]);
-  std::cout << "[0x0020-0x0027] Newest Chunk Offset: 0x" << std::hex
+  std::cout << "[0x0020-0x0027] Newest Chunk Offset: 0x" << std::hex << newest_offset << "\n";
+  
+  // Checksum (0x0028,
