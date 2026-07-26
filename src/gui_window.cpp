f5cf2a1d8bded421331a6babd5357df63979a964
windows123fish <xhoddh@outlook.com>
2026-07-26 16:38:05 +0800

    dev: update 周日 2-26-07_16:38:04

--- src/gui_window.cpp
+++ src/gui_window.cpp
@@ -131,4 +131,5 @@ void GuiWindow::render() {
     
     // Main window
     ImGui::SetNextWindowPos(ImVec2(0, 0));
-    ImGui::SetNextWindowSize(ImGui::GetIO().
+    ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
+    ImGui::Begin("EvtxXtract - EVTX文件解析器", NULL, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove
