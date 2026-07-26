8fe4feb8992cce431288d5f7b1701891f0f492d9
windows123fish <xhoddh@outlook.com>
2026-07-26 15:35:13 +0800

    dev: update 周日 2-26-07_15:35:13

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -91,3 +91,6 @@ struct EVT_CHUNK_HEADER {
 
 constexpr size_t EVTX_FILE_HEADER_SIZE = 4096;
 constexpr size_t EVTX_CHUNK_SIZE = 65536;
+constexpr size_t EVTX_CHUNK_HEADER_SIZE = 512;
+
+constexpr std
