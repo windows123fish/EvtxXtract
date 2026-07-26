578b8fe2849367944fa4c00c4103630f57f89fcc
windows123fish <xhoddh@outlook.com>
2026-07-26 16:36:09 +0800

    dev: update 周日 2-26-07_16:36:08

--- src/gui_window.cpp
+++ src/gui_window.cpp
@@ -151,4 +151,6 @@ void GuiWindow::render() {
 }
 
 void GuiWindow::renderFileSelection() {
-    ImGui::BeginChild("文件选择", ImVec2(300, ImGui::GetWindowHeight() - 100),
+    ImGui::BeginChild("文件选择", ImVec2(300, ImGui::GetWindowHeight() - 100), true);
+    
+    ImGui::TextColored(ImVec4(0.4f,
