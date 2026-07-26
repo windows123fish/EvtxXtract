6082b8032cd1b3679d86239a7b61416079089860
windows123fish <xhoddh@outlook.com>
2026-07-26 16:35:57 +0800

    dev: update 周日 2-26-07_16:35:56

--- src/gui_window.cpp
+++ src/gui_window.cpp
@@ -61,4 +61,5 @@ bool GuiWindow::init(HINSTANCE hInstance, const std::string& title, int width, i
     );
 
     if (!m_hWnd) {
-        MessageBoxA(NULL,
+        MessageBoxA(NULL, "窗口创建失败", "错误", MB_OK | MB_ICONERROR);
+        return false
