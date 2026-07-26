cb46b79851d2caffc8356439826f99974b222206
windows123fish <xhoddh@outlook.com>
2026-07-26 15:33:59 +0800

    dev: update 周日 2-26-07_15:33:58

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -29,3 +29,5 @@ struct EVT_FILE_HEADER {
 struct EVT_CHUNK_HEADER {
   std::array<uint8_t, 8> magic;
   uint64_t first_event_record_number;
+  uint64_t last_event_record_number;
+  uint64_t first_event_record_id
