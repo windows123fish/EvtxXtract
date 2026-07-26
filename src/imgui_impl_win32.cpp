488f1aab4a7561e6be84ad4488c69d220b8503d6
windows123fish <xhoddh@outlook.com>
2026-07-26 16:34:41 +0800

    dev: update 周日 2-26-07_16:34:40

--- src/imgui_impl_win32.cpp
+++ src/imgui_impl_win32.cpp
@@ -170,4 +170,5 @@ static void ImGui_ImplWin32_UpdateMouseCursor() {
 }
 
 // WndProc handler
-IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam,
+IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    if (ImGui::GetCurrentContext() == NULL)
