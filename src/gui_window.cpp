87890284f93769ecf166a556a0245d7de05c3b4d
windows123fish <xhoddh@outlook.com>
2026-07-26 16:50:03 +0800

    dev: update 周日 2-26-07_16:50:02

--- src/gui_window.cpp
+++ src/gui_window.cpp
@@ -256,4 +256,7 @@ void GuiWindow::renderDrawData(ImDrawData* draw_data) {
     // Draw to screen
     HBITMAP oldBitmap = (HBITMAP)SelectObject(m_hDC, hBitmap);
     BitBlt(m_hDC, 0, 0, width, height, NULL, 0, 0, SRCCOPY);
-    Select
+    SelectObject(m_hDC, oldBitmap);
+}
+
+void GuiWindow::renderFileSelection() {
