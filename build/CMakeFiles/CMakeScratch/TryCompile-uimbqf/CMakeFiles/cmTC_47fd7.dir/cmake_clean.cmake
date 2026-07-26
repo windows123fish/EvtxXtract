bcaf170a8a60bcf378606962b47aceac1430ee95
windows123fish <xhoddh@outlook.com>
2026-07-26 16:52:07 +0800

    dev: update 周日 2-26-07_16:52:06

---
+++ build/CMakeFiles/CMakeScratch/TryCompile-uimbqf/CMakeFiles/cmTC_47fd7.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_47fd7.dir/CMakeCXXCompilerABI.cpp.obj"
+  "cmTC_47fd7.exe"
+  "cmTC_47fd7.exe.manifest"
+  "cmTC_47fd7.pdb"
+  "libcmTC_47fd7.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_47fd7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
