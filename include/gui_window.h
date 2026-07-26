b9d49264c6fa837914d32c4f72cf0e02bf38fc2f
windows123fish <xhoddh@outlook.com>
2026-07-26 16:39:04 +0800

    dev: update 周日 2-26-07_16:39:00

--- include/gui_window.h
+++ include/gui_window.h
@@ -1,3 +1,49 @@
 #pragma once
 
-#include <windows.h
+#include <windows.h>
+#include <string>
+#include <vector>
+#include "evtx_structs.h"
+
+struct ChunkInfo {
+    uint64_t offset;
+    uint32_t event_count;
+    uint32_t checksum;
+};
+
+class GuiWindow {
+public:
+    GuiWindow();
+    ~GuiWindow();
+    
+    bool init(HINSTANCE hInstance, const std::string& title, int width, int height);
+    void shutdown();
+    void run();
+    
+private:
+    void render();
+    void renderFileSelection();
+    void renderFileInfo();
+    void renderFooter();
+    
+    void scanEvtxFiles();
+    void parseSelectedFile();
+    
+    LRESULT wndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+    static LRESULT CALLBACK s_wndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+    
+private:
+    HWND m_hWnd;
+    HDC m_hDC;
+    bool m_isRunning;
+    
+    // File selection
+    std::vector<std::string> m_evtxFiles;
+    int m_selectedFileIndex;
+    
+    // Parsing state
+    bool m_isParsing;
+    Evtx::EVT_FILE_HEADER m_fileHeader;
+    std::vector<ChunkInfo> m_chunkInfo;
+    uint32_t m_validChunkCount;
+};
