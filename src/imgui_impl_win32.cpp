c079f3f95b8652e96f65510fa9eff4bf990a4e61
windows123fish <xhoddh@outlook.com>
2026-07-26 16:34:48 +0800

    dev: update 周日 2-26-07_16:34:47

--- src/imgui_impl_win32.cpp
+++ src/imgui_impl_win32.cpp
@@ -207,4 +207,7 @@ IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARA
             return 0;
         case WM_KEYUP:
             if (wParam < 256)
-                io.KeysDown[w
+                io.KeysDown[wParam] = false;
+            return 0;
+        case WM_CHAR:
+            if
