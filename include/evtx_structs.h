416d1a39a9ca7a8857a91b24006070b5e7305e15
windows123fish <xhoddh@outlook.com>
2026-07-26 15:10:40 +0800

    dev: update 周日 2-26-07_15:10:39

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -11,3 +11,7 @@ namespace Evtx {
 // Section 2.2: Chunk Header Structure (512 bytes)
 
 // Ensure no padding between struct members
+#pragma pack(push, 1)
+
+/**
+ * @brief EVT_FILE_HEADER structure
