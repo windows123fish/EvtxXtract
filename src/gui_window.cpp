94d9fd3d3f924a7c7008e328e18f8974f47c4733
windows123fish <xhoddh@outlook.com>
2026-07-26 16:35:52 +0800

    dev: update 周日 2-26-07_16:35:52

--- src/gui_window.cpp
+++ src/gui_window.cpp
@@ -36,4 +36,6 @@ bool GuiWindow::init(HINSTANCE hInstance, const std::string& title, int width, i
     WNDCLASSEXA wc = {0};
     wc.cbSize        = sizeof(WNDCLASSEXA);
     wc.style         = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
-    wc.lpfnWndProc   = s_w
+    wc.lpfnWndProc   = s_wndProc;
+    wc.hInstance     = hInstance;
+    wc.hCursor       =
