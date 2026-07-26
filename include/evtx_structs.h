fcc90235374b2870b667a1dc69ff6342f3d887ad
windows123fish <xhoddh@outlook.com>
2026-07-26 15:23:38 +0800

    dev: update 周日 2-26-07_15:23:38

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -64,3 +64,6 @@ struct EVT_FILE_HEADER {
    * @brief Number of chunks in the file (2 bytes, little-endian)
    */
   uint16_t chunk_count;
+
+  /**
+   * @brief File size in bytes (8 bytes, little-endian)
