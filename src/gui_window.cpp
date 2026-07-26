730551eff6e12c992585f12a7de191796bf90c6e
windows123fish <xhoddh@outlook.com>
2026-07-26 16:37:44 +0800

    dev: update 周日 2-26-07_16:37:43

--- src/gui_window.cpp
+++ src/gui_window.cpp
@@ -7,4 +7,5 @@
 #include <filesystem>
 
 // Forward declarations
-extern IMGUI_IMPL_API bool ImGui_ImplWin3
+extern IMGUI_IMPL_API bool ImGui_ImplWin32_Init(HWND hWnd);
+extern IMGUI_IMPL_API void ImGui_ImplWin
