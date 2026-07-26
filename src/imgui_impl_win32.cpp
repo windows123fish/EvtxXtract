226b2fba4f7b5bccfba4a6e0e7ca991e55f30b8b
windows123fish <xhoddh@outlook.com>
2026-07-26 16:34:38 +0800

    dev: update 周日 2-26-07_16:34:38

--- src/imgui_impl_win32.cpp
+++ src/imgui_impl_win32.cpp
@@ -159,4 +159,5 @@ static void ImGui_ImplWin32_UpdateMouseCursor() {
                 case ImGuiMouseCursor_ResizeNS:     cursor_id = IDC_SIZENS; break;
                 case ImGuiMouseCursor_ResizeEW:     cursor_id = IDC_SIZEWE; break;
                 case ImGuiMouseCursor_ResizeNESW:   cursor_id = IDC_SIZENESW; break;
-                case ImGuiMouseCursor_ResizeNWSE:
+                case ImGuiMouseCursor_ResizeNWSE:   cursor_id = IDC_SIZENWSE; break;
+                case ImGuiMouseCursor_
