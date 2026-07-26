25c60a8a2b1d8ac6554da189d44411437a307667
windows123fish <xhoddh@outlook.com>
2026-07-26 15:35:13 +0800

    dev: update 周日 2-26-07_15:35:13

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -93,4 +93,4 @@ constexpr size_t EVTX_FILE_HEADER_SIZE = 4096;
 constexpr size_t EVTX_CHUNK_SIZE = 65536;
 constexpr size_t EVTX_CHUNK_HEADER_SIZE = 512;
 
-constexpr std
+constexpr std::array<uint8_t, 8> EV
