e440deb8a25ad2354333a8e342392849583df1a1
windows123fish <xhoddh@outlook.com>
2026-07-26 16:35:56 +0800

    dev: update 周日 2-26-07_16:35:55

--- src/gui_window.cpp
+++ src/gui_window.cpp
@@ -56,3 +56,5 @@ bool GuiWindow::init(HINSTANCE hInstance, const std::string& title, int width, i
         title.c_str(),
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT,
+        rect.right - rect.left, rect.bottom - rect.top,
+        NULL, NULL, hInstance
