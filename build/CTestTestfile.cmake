430f8b3fa69ad381def97e4385254140fde2fab2
windows123fish <xhoddh@outlook.com>
2026-07-26 17:06:04 +0800

    dev: update 周日 2-26-07_17:04:00

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/main/EvtxXtract
-# Build directory: D:/main/EvtxXtract/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
