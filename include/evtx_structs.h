95c39ff47a2b11887ef063ae5e585461166dcf0c
windows123fish <xhoddh@outlook.com>
2026-07-26 15:34:00 +0800

    dev: update 周日 2-26-07_15:34:00

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -34,4 +34,6 @@ struct EVT_CHUNK_HEADER {
   uint64_t last_event_record_id;
   uint32_t header_size;
   uint32_t last_event_offset;
-  uint32_t free_space_offset
+  uint32_t free_space_offset;
+  uint32_t events_checksum;
+  uint32_t unknown1;
