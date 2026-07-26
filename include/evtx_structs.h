a93aa583307e3c9322113e4c9279ae32c862e87f
windows123fish <xhoddh@outlook.com>
2026-07-26 15:29:57 +0800

    dev: update 周日 2-26-07_15:29:57

--- include/evtx_structs.h
+++ include/evtx_structs.h
@@ -244,4 +244,9 @@ struct EVT_CHUNK_HEADER {
   /**
    * @brief Validate the chunk header magic number
    * 
-   * @return true if magic matches "ElfChnk\x00", false
+   * @return true if magic matches "ElfChnk\x00", false otherwise
+   */
+  bool validate_magic() const noexcept;
+
+  /**
+   *
