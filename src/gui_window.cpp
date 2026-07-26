a6f15355f02ef482efcd019b8875a82a1c419038
windows123fish <xhoddh@outlook.com>
2026-07-26 16:36:16 +0800

    dev: update 周日 2-26-07_16:36:16

--- src/gui_window.cpp
+++ src/gui_window.cpp
@@ -191,3 +191,6 @@ void GuiWindow::renderFileInfo() {
     
     ImGui::TextColored(ImVec4(0.6f, 0.8f, 1.0f, 1.0f), "📋 文件信息");
     ImGui::Separator();
+    
+    if (!m_isParsing && m_fileHeader.magic[0] == 0) {
+        ImGui::Text("请选择一个
