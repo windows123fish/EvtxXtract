7c48eb63450cbd47098752793e2982c40f40bd5b
windows123fish <xhoddh@outlook.com>
2026-07-26 16:36:27 +0800

    dev: update 周日 2-26-07_16:36:27

--- src/gui_window.cpp
+++ src/gui_window.cpp
@@ -231,4 +231,6 @@ void GuiWindow::renderFileInfo() {
             ImGui::TableSetupColumn("校验和");
             ImGui::TableHeadersRow();
             
-            for (const auto& info : m_chunkInfo)
+            for (const auto& info : m_chunkInfo) {
+                ImGui::TableNextRow();
+                ImGui::TableSetColumnIndex(0);
