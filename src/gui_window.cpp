e1f0168628980f05bc1faed02254d2135bf5dcfd
windows123fish <xhoddh@outlook.com>
2026-07-26 16:36:11 +0800

    dev: update 周日 2-26-07_16:36:11

--- src/gui_window.cpp
+++ src/gui_window.cpp
@@ -162,4 +162,5 @@ void GuiWindow::renderFileSelection() {
             scanEvtxFiles();
         }
     } else {
-        for (size_t i = 0; i < m_evtxFiles
+        for (size_t i = 0; i < m_evtxFiles.size(); i++) {
+            bool isSelected = (m_selectedFileIndex == (int
